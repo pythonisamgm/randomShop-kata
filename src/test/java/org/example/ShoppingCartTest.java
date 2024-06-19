06995975de03b9a684451ac7c871687c6353b5f5
Marta Gonzalez Morales <pythonisamgm@gmail.com>
2024-06-19 13:15:12 +0200

    modify tests' names and add tests for magic cards and fishes

--- src/test/java/org/example/ShoppingCartTest.java
+++ src/test/java/org/example/ShoppingCartTest.java
@@ -2,11 +2,13 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ShoppingCartTest {
     @Test
-    void calculatePriceForMagicCards_red() {
+    void if_magic_card_is_red_return_3point5() {
         ShoppingCart shoppingCart = new ShoppingCart();
 
         Product product = new Product(null, null, false, "red", null, "Magic: The Gathering - Lightning Bolt", null);
@@ -18,7 +20,7 @@ class ShoppingCartTest {
     }
 
     @Test
-    void calculatePriceForMagicCards_blue() {
+    void if_magic_card_is_blue_return_5() {
         ShoppingCart shoppingCart = new ShoppingCart();
 
         Product product = new Product(null, null, false, "blue", null, "Magic: The Gathering - Maga Eternal", null);
@@ -28,9 +30,53 @@ class ShoppingCartTest {
         assertEquals(shoppingCart.getTotalPrice(), 5.0);
 
     }
+    @Test
+    void if_magic_card_is_green_return_4point4() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+
+        Product product = new Product(null, null, false, "green", null, "Magic: The Gathering - Maga Eternal", null);
+
+        shoppingCart.addProduct(product);
+
+        assertEquals(shoppingCart.getTotalPrice(), 4.4);
+
+    }
+    @Test
+    void if_magic_card_is_black_return_6point8() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+
+        Product product = new Product(null, null, false, "black", null, "Magic: The Gathering - Maga Eternal", null);
+
+        shoppingCart.addProduct(product);
+
+        assertEquals(shoppingCart.getTotalPrice(), 6.8);
+
+    }
+    @Test
+    void if_magic_card_is_brown_return_2() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+
+        Product product = new Product(null, null, false, "brown", null, "Magic: The Gathering - Maga Eternal", null);
+
+        shoppingCart.addProduct(product);
+
+        assertEquals(shoppingCart.getTotalPrice(), 2);
+
+    }
+    @Test
+    void if_magic_card_is_blackLotus_return_40000() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+
+        Product product = new Product(null, null, false, "", null, "Magic: The Gathering - Black Lotus", null);
+
+        shoppingCart.addProduct(product);
+
+        assertEquals(shoppingCart.getTotalPrice(), 40000);
+
+    }
 
     @Test
-    void calculatePriceForWine() {
+    void if_product_is_wine_multiply_age_in_year_per_20() {
         ShoppingCart shoppingCart = new ShoppingCart();
 
         Product product = new Product(null, 5, false, null, null, "el tio juanillo", null);
@@ -42,7 +88,7 @@ class ShoppingCartTest {
     }
 
     @Test
-    void calculatePriceForCheese() {
+    void if_product_is_stinky_cheese_multiply_age_in_year_per_10() {
         ShoppingCart shoppingCart = new ShoppingCart();
 
         Product product = new Product(null, 5, true, null, null, "French Camembert", null);
@@ -50,6 +96,32 @@ class ShoppingCartTest {
         shoppingCart.addProduct(product);
 
         assertEquals(shoppingCart.getTotalPrice(), 50.0);
+
+    }
+    //set basePrice to 0
+    @Test
+    void if_product_is_blue_fish_add_0point1_to_basePrice() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+        BigDecimal zero = new BigDecimal(0);
+
+        Product product = new Product(null, null, false, "blue", zero, "", null);
+
+        shoppingCart.addProduct(product);
+
+        assertEquals(shoppingCart.getTotalPrice(), 0.1);
+
+    }
+    //set basePrice to 1
+    @Test
+    void if_product_is_golden_fish_multiply_100_for_basePrice() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+        BigDecimal one = new BigDecimal(1);
+
+        Product product = new Product(null, null, false, "gold", one, "", null);
+
+        shoppingCart.addProduct(product);
+
+        assertEquals(shoppingCart.getTotalPrice(), 100.0);
 
     }
 }
